3e535dee454861dd17a0124846344bc0672b3859
Itai Y. Efrat <itai3397@gmail.com>
2020-08-29 11:07:38 +0300

    Queue improvements, metadata overhaul

--- src/Song.hs
+++ src/Song.hs
@@ -7,41 +7,22 @@ import           Data.Maybe                     ( fromJust )
 import           System.IO.Unsafe
 import           Data.Text                     as T
 import           Text.Printf                    ( printf )
-
+{-
 -- | test song, to be destroyed
 soong :: MPD.Song
 soong =
   unsafePerformIO $ fromJust . fromRight Nothing <$> MPD.withMPD MPD.currentSong
+-}
 
-type MetaQuery = MPD.Song -> Maybe Text
 
 -- | Get comma seperated metedata from tag
-metaLookup :: MPD.Metadata -> MPD.Song -> Maybe Text
-metaLookup tag =
-  (T.intercalate "," <$>) . (MPD.toText <<$>>) . Map.lookup tag . MPD.sgTags
+meta :: Text -> MPD.Metadata -> MPD.Song -> Text
+meta notFound tag song = maybe
+  notFound
+  (T.intercalate ",")
+  (MPD.toText <<$>> Map.lookup tag (MPD.sgTags song))
 
-
--- | Song tag queries
-artist, artistSort, album, albumArtist, albumArtistSort, title, track, name, genre, date, composer, performer, comment, disc
-  :: MetaQuery
-artist = metaLookup MPD.Artist
-artistSort = metaLookup MPD.ArtistSort
-album = metaLookup MPD.Album
-albumArtist = metaLookup MPD.AlbumArtist
-albumArtistSort = metaLookup MPD.AlbumArtistSort
-title = metaLookup MPD.Title
-track = metaLookup MPD.Track
-name = metaLookup MPD.Name
-genre = metaLookup MPD.Genre
-date = metaLookup MPD.Date
-composer = metaLookup MPD.Composer
-performer = metaLookup MPD.Performer
-comment = metaLookup MPD.Comment
-disc = metaLookup MPD.Disc
-
--- | Song info queries
-duration :: MetaQuery
-
-duration song =
-  let (minutes, seconds) = divMod (MPD.sgLength song) 60
-  in  Just $ T.pack $ printf "%d:%02d" minutes seconds
+secondsToTime :: Integer -> Text
+secondsToTime sec =
+  let (minutes, seconds) = divMod sec 60
+  in  T.pack $ printf "%d:%02d" minutes seconds
--- src/UI.hs
+++ src/UI.hs
@@ -46,7 +46,9 @@ app = App
   , appStartEvent   = pure
   , appAttrMap      = const $ attrMap
                         defAttr
-                        [(listSelectedAttr, Vty.black `Brick.Util.on` Vty.white)]
+                        [ (listSelectedAttr, Vty.black `Brick.Util.on` Vty.white)
+                        , (attrName "header", Vty.withStyle defAttr Vty.underline)
+                        ]
   }
 
 buildInitialState :: IO HState
@@ -60,41 +62,70 @@ buildInitialState = do
 
 drawSong :: HState -> Widget Name
 drawSong st =
-  vLimit 3 . center . borderWithLabel (str "Now Playing") . txt $ fromMaybe
-    "draw ded."
-    (title =<< currentSong st)
+  vLimit 3 . center . borderWithLabel (str "Now Playing") $ fromMaybe
+    (txt "nothing.")
+    (queueRow <$> (currentSong st))
 
 drawPlaylist :: HState -> Widget Name
 drawPlaylist st =
-  let vsize = case (queueExtent st) of
+  let vsize = case queueExtent st of
         Just e  -> (snd . extentSize $ e) - 2
         Nothing -> 40
   in  reportExtent Queue
-        $ borderWithLabel (str "Queue (under construction)")
-        $ viewport Queue Vertical
-        $ visible
-        $ vLimit vsize
-        . center
-        $ playlistWidget
-            (   (fromMaybe "" .)
-            <$> [ album
-                , title
-                , artist
-                , ((\(MPD.Id x) -> show x) <$>) . MPD.sgId
-                , (show <$>) . MPD.sgIndex
-                ]
-            )
-            (queue st)
+        $   borderWithLabel (str "Queue (under construction)")
+        $   viewport Queue Vertical
+        $   visible
+        $   vLimit vsize
+        .   center
+        $   header
+        <=> (renderList (const queueRow) True (queue st))
+
+ where
+  songIdx  = column (Just 4) Max (Pad 1) $ txt "Inx"
+  songId   = column (Just 3) Max (Pad 1) $ txt "ID"
+  album    = column (Just 25) (Pad 1) Max $ txt "Album"
+  track    = column (Just 8) Max (Pad 2) $ txt "#"
+  title    = column Nothing Max Max $ txt "Title"
+  artist   = column (Just 25) Max (Pad 1) $ txt "Artist"
+  duration = column (Just 8) Max (Pad 1) $ txt "Time"
+  header   = withAttr
+    ("header")
+    (songIdx <+> songId <+> album <+> track <+> title <+> artist <+> duration)
+
+queueRow :: MPD.Song -> Widget n
+queueRow song =
+  hCenter
+    $   songIdx
+    <+> songId
+    <+> album
+    <+> track
+    <+> title
+    <+> artist
+    <+> duration
+ where
+  songIdx =
+    column (Just 4) Max (Pad 1) $ txt $ maybe "?" show $ MPD.sgIndex song
+  songId =
+    column (Just 3) Max (Pad 1)
+      $ txt
+      $ maybe "?" (\(MPD.Id x) -> show x)
+      $ MPD.sgId song
+  album = column (Just 25) (Pad 1) Max $ txt $ meta "<no album>" MPD.Album song
+  track = column (Just 8) Max (Pad 2) $ txt $ meta "?" MPD.Track song
+  title = column Nothing Max Max $ txt $ meta "<no title>" MPD.Title song
+  artist =
+    column (Just 25) Max (Pad 1) $ txt $ meta "<no artist>" MPD.Artist song
+  duration =
+    column (Just 8) Max (Pad 1) $ txt $ secondsToTime $ MPD.sgLength song
+
+column :: Maybe Int -> Padding -> Padding -> Widget n -> Widget n
+column maxWidth left right w = case maxWidth of
+  Nothing -> wpad
+  Just m  -> hLimit m wpad
+  where wpad = padLeft left . padRight right $ w
 
 drawUI :: HState -> [Widget Name]
 drawUI st = [(<=>) (drawPlaylist st) (drawSong st)]
-
-
-playlistWidget :: [MPD.Song -> Text] -> (List Name MPD.Song) -> Widget Name
-playlistWidget fs songs = renderList (const $ hBoxPad (Max) . (txt <$>))
-                                     True
-                                     (fmap (\song -> fs ?? song) songs)
- -- hBoxPad (Pad 1) $ fmap (\f -> vBox $ txt <$> (f <$> songs)) fs
 
 hBoxPad :: Padding -> [Widget n] -> Widget n
 hBoxPad _ []       = emptyWidget
