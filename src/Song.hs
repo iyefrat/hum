e0a30810ed572f0db4ea3695d16b1455ab700612
Itai Y. Efrat <itai3397@gmail.com>
2020-08-29 11:50:45 +0300

    soong destroyed

--- src/Song.hs
+++ src/Song.hs
@@ -3,17 +3,8 @@
 module Song where
 import qualified Network.MPD                   as MPD
 import qualified Data.Map.Strict               as Map
-import           Data.Maybe                     ( fromJust )
-import           System.IO.Unsafe
 import           Data.Text                     as T
 import           Text.Printf                    ( printf )
-{-
--- | test song, to be destroyed
-soong :: MPD.Song
-soong =
-  unsafePerformIO $ fromJust . fromRight Nothing <$> MPD.withMPD MPD.currentSong
--}
-
 
 -- | Get comma seperated metedata from tag
 meta :: Text -> MPD.Metadata -> MPD.Song -> Text
