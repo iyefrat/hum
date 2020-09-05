109ba3132d55862f45e01613c448a0520bd57301
Itai Y. Efrat <itai3397@gmail.com>
2020-09-05 16:40:39 +0300

    Library imports

--- src/Ham/Views/Library.hs
+++ src/Ham/Views/Library.hs
@@ -1,3 +1,12 @@
 -- |
 
 module Ham.Views.Library where
+import           Ham.Types
+import           Brick.Types
+import           Brick.Widgets.Core
+import           Brick.Widgets.Center
+import           Brick.Widgets.Border
+import           Brick.Widgets.List
+import           Ham.Song
+import           Ham.Attributes
+import qualified Network.MPD                   as MPD
