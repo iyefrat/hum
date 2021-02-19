c80b6acf09f18a15aaa29b8ae74da7ca5e22716c
Itai Y. Efrat <itai3397@gmail.com>
2021-02-20 00:50:15 +0200

    add --version flag

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,44 @@
 
+-- | Module    : Main
+-- Copyright   : (c) Itai Y. Efrat 2020-2021
+-- License     : GPLv2-or-later (see LICENSE)
+-- Maintainer  : Itai Y. Efrat <itai3397@gmail.com>
+--
+-- The necessary logic to launch the hum executable, including commandline
+-- arguments.
+
 module Main where
 
-import           Brick.Main
 import qualified Brick.BChan                   as BC
+import           Brick.Main
+import           Control.Concurrent             ( forkIO
+                                                , threadDelay
+                                                )
 import qualified Graphics.Vty                  as Vty
-import           Hum.UI                         ( buildInitialState
-                                                , app
+import           Hum.Types               hiding ( help )
+import           Hum.UI                         ( app
+                                                , buildInitialState
                                                 )
-import           Control.Concurrent             ( threadDelay
-                                                , forkIO
+import           Network.MPD                    ( idle
+                                                , withMPD
                                                 )
-import           Hum.Types
-import           Network.MPD                    ( withMPD
-                                                , idle
-                                                )
+import           Options.Applicative
+
 -- | Calls functions from "Hum.UI", go there next.
 main :: IO ()
 main = do
-  chan         <- BC.newBChan 10
-  _            <- forkIO $ mpdListenForever chan
-  _            <- forkIO $ tickTock chan
-  initialState <- buildInitialState chan
-  let buildVty = Vty.mkVty Vty.defaultConfig
-  initialVty <- buildVty
-  _          <- customMain initialVty buildVty (Just chan) app initialState
-  pass
+  arg <- execParser opts
+  case arg of
+    HumArgs { version = True } -> putStrLn "hum 0.2.0.0"
+    _                       -> do
+      chan         <- BC.newBChan 10
+      _            <- forkIO $ mpdListenForever chan
+      _            <- forkIO $ tickTock chan
+      initialState <- buildInitialState chan
+      let buildVty = Vty.mkVty Vty.defaultConfig
+      initialVty <- buildVty
+      _          <- customMain initialVty buildVty (Just chan) app initialState
+      pass
 
 -- | Channel that waits for mpd events.
 mpdListenForever :: BC.BChan HumEvent -> IO ()
@@ -37,3 +51,15 @@ tickTock :: BC.BChan HumEvent -> IO ()
 tickTock chan = forever $ do
   BC.writeBChan chan (Left Tick)
   threadDelay 1000000
+
+data HumArgs = HumArgs
+  { version :: Bool
+  }
+
+args :: Parser HumArgs
+args = HumArgs <$> switch (long "version" <> short 'v' <> help "Hum's version")
+
+opts :: ParserInfo HumArgs
+opts = info (args <**> helper)
+            (fullDesc {-<> progDesc "Print a greeting for TARGET"-}
+                      <> header "Hum - a haskell music manager")
